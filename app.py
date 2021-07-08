a90db9c2de8ce1c593147e6c6afcc7316e025275
stocktons <sarah@thanksforallthe.fish>
2021-07-08 15:35:17 -0400

    completed steps 1 and 2

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+# as the user answers questions, their answers get stored in here:
+responses = []
+
+@app.route('/')
+def survey():
+    """Shows the title, the instructions, and a button to start the survey. Directs user to questions/0"""
+
+
+    return render_template(
+        "survey_start.html",
+        #title = survey.title
+    )
+
+#@app.route('/questions/0')
